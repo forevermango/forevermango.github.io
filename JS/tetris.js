0dcb8bcfde6f2239321c9a35c2612b9c6dce4089
forevermango <raahima95@gmail.com>
2020-10-15 11:05:22 -0500

    made first tetrominoes

---
+++ JS/tetris.js
@@ -0,0 +1,16 @@
+
+
+const $cvs = document.getElementById("tetris");
+
+const ctx = cvs.getContext("2d");
+
+const row = 20;
+const col = column = 10; 
+const sq = squareSize = 20; 
+const empty = "white";
+
+$(() => {
+    const cvs = $("#tatris")
+
+
+})
